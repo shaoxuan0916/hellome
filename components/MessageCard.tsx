e64c043538280e09aa9cca4cd416fab06d19f1c2
shaoxuan <shaoxuanhinhua@gmail.com>
2022-11-07 22:00:53 +0800

    first commit

---
+++ components/MessageCard.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const MessageCard = () => {
+  return (
+    <div className="bg-red-300">
+      <div className="max-w-[1200px] mx-auto">hello</div>
+    </div>
+  )
+}
+
+export default MessageCard
