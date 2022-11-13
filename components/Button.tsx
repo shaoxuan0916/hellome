5fef727c7ebc88bf2a10b8f44ddf788aed2a2858
shaoxuan <shaoxuanhinhua@gmail.com>
2022-11-13 17:37:45 +0800

    update sign up page

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,16 @@
-import { NextPage } from 'next'
-import React from 'react'
+import { NextPage } from "next"
+import React from "react"
 
-interface IButtonProps{
-    text: string
+interface IButtonProps {
+  text: string
 }
 
-const Button: NextPage<IButtonProps> = ({text}) => {
+const Button: NextPage<IButtonProps> = ({ text }) => {
   return (
-    <div className='w-full'>
-        <button className='bg-primary flex justify-center py-2 rounded-md cursor-pointer hover:bg-primaryHover'>
-            <p className='text-[#fff] text-xl font-semibold'>{text}</p>
-        </button>
+    <div className="w-full">
+      <div className="bg-primary flex justify-center py-2 rounded-md cursor-pointer hover:bg-primaryHover">
+        <p className="text-[#fff] text-xl font-semibold">{text}</p>
+      </div>
     </div>
   )
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,37 +1,34 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import logo from "../public/logo-green.png";
-import Input from "../components/Input";
-import Button from "../components/Button";
-import Link from "next/link";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { auth } from "../firebase";
+import React, { useState } from "react"
+import Image from "next/image"
+import logo from "../public/logo-green.png"
+import Input from "../components/Input"
+import Button from "../components/Button"
+import Link from "next/link"
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
+import { auth } from "../firebase"
 
 const SignUpPage = () => {
-  const [error, setError] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   // const [username, setUsername] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const auth = getAuth();
+  const [confirmPassword, setConfirmPassword] = useState("")
+  // const auth = getAuth()
   const handleSignUp = (e: any) => {
-    console.log("Success");
-    console.log(email);
-    console.log(password);
-    console.log(auth);
-    e.preventDefault();
-    
+    console.log("Success")
+    e.preventDefault()
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user;
-        console.log(user);
+        const user = userCredential.user
+        console.log(user)
         // ...
       })
       .catch((error) => {
-        setError(true);
-      });
-  };
+        setError(true)
+      })
+  }
   return (
     <div className=" h-[100vh] ">
       <div className="h-[25%] bg-secondary flex items-center justify-center">
@@ -44,31 +41,27 @@ const SignUpPage = () => {
         <h3 className="text-4xl font-semibold">Sign Up</h3>
 
         <div className="mt-8">
+          {error && <div>User Already Exist</div>}
           {/* <Input label="Your Name" placeholder="This is your display name" /> */}
           {/* <Input label="Username" placeholder="min 6 characters" /> */}
-          <form onSubmit={handleSignUp}>
-          <Input label="Email" placeholder="Your Email" value={email} setValue={setEmail}/>
+          <Input label="Email" placeholder="Your Email" setValue={setEmail} />
           <Input
             label="Password"
             placeholder="min 8 charaters include upper and lower case"
-            value={password} 
             setValue={setPassword}
           />
+
           {/* <Input label="Confirm Password" placeholder="Re-type your password" value={confirmPassword} setValue={setConfirmPassword} /> */}
-          {/* <div onClick={handleSignUp}>
+          <div onClick={handleSignUp}>
             <Button text="Sign Up" />
-          </div> */}
-          <button type="submit">
-            Sign Up
-          </button>
+          </div>
           <div className="pt-2 text-primary">
             <Link href="/login">Already Have An Account? Login Now</Link>
           </div>
-          </form>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SignUpPage;
+export default SignUpPage
