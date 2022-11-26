422c658522700c180f5c9a6c44d048d1d38168bd
shaoxuan <shaoxuanhinhua@gmail.com>
2022-11-26 23:00:01 +0800

    something

--- pages/home/[id].tsx
+++ pages/home/[id].tsx
@@ -1,13 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import NavBar from "../../components/NavBar"
 import useAuthStore from "../../store/authStore"
 import { auth } from "../../firebase"
+import { useRouter } from "next/router"
 
 const HomePage = () => {
   const { userProfile } = useAuthStore()
+  const router = useRouter()
+  const [show, setShow] = useState(false)
 
   console.log("userProfile", userProfile)
-  console.log("auth", auth)
+  // @ts-ignore
+  // console.log("auth", auth.lastNotifiedUid)
+
+  const currentUid = window.location.pathname.substring(6)
+
+  useEffect(() => {
+    // @ts-ignore
+    if (currentUid !== auth.lastNotifiedUid) {
+      router.push("/login")
+      setShow(true)
+    }
+  }, [])
 
   return (
     <div className="max-w-[600px] mx-auto">
